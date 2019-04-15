5abd5c8a4efca44b5902948ac640754def3cf1d5
Branchaev Ivan <ivanbranchaev@bk.ru>
2019-04-16 00:20:14 +0400

    Init

---
+++ src/main/java/com/example/projectrepo/ProjectrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectrepoApplication.class, args);
+    }
+
+}
